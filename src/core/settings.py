5c2046bcaaf3c2da238e89c224444b8d3393697c
DamnGachi <112755006+DamnGachi@users.noreply.github.com>
2023-01-26 14:11:25 +0300

    Delete settings.py

--- src/core/settings.py
+++
@@ -1,4 +0,0 @@
-"""File with settings and configs for the project"""
-
-DATABASE_URL = "postgresql+asyncpg://postgres:Daimondi12@localhost:5432/Nevless"
-TEST_DATABASE_URL = "postgresql+asyncpg://postgres:Daimondi12@localhost:5432/Nevless"
